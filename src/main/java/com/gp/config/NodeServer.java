74c793d5bd037d6f51ca843e81cafd2724332761
diaogc <gary.diao@yahoo.com>
2022-01-02 23:57:31 +0800

    update

---
+++ src/main/java/com/gp/config/NodeServer.java
@@ -0,0 +1,23 @@
+package com.gp.config;
+
+import com.gp.asm.AgentBinder;
+import com.gp.db.asm.JdbiTransformer;
+import com.networknt.server.Server;
+
+public class NodeServer {
+
+    public static void main(String[] args) throws Exception {
+
+        // initial agent
+        initialAgent();
+
+        Server webServer = new Server();
+        webServer.init();
+    }
+
+    // try to initial the java agent
+    public static void initialAgent() throws Exception {
+        JdbiTransformer transformer = new JdbiTransformer();
+        AgentBinder.bindAgent(transformer);
+    }
+}
