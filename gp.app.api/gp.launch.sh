5170c8f8dbe44b12efb3b98e4f176bde619da99b
diaogc <gary.diao@yahoo.com>
2024-07-16 14:07:51 +0800

    commit

--- gp.app.api/gp.launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo ++ try to launch war ++
-GP_HOME=`pwd`
-CP=$GP_HOME/target/gp.web-0.1.war 
-LP=$GP_HOME/target/lib
-echo $CP
-echo $LP
-java -cp $CP -Dloader.path=$CP!BOOT-INF/classes/WEB-INF/classes,$CP!BOOT-INF/classes/WEB-INF,$LP org.springframework.boot.loader.PropertiesLauncher
